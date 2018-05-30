053d71529b2dada47dbda63029ed9e0fc0d99412
Denise Pendleton <dpendl1@hotmail.com>
2018-05-30 12:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+class Driver {
+  constructor(name, day){
+    this.name = name
+    this.day = day
+    this.startDate = new Date(day)
+   }
+
+   yearsExperienceFromBeginningOf(endDate){
+     return endDate - this.startDate.getFullYear()
+   }
+  }
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    const horizontalTravelled = Math.abs(this.endingLocation.horizontal - this.beginningLocation.horizontal);
+    const verticalTravelled = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+
+    return horizontalTravelled + verticalTravelled;
+  }
+}
